5332a0281a78f9203ae137b0c6b053ccdefbf30f
Kensy Josabeth Ayala <kensy.ayala1994@gmail.com>
2022-03-17 19:36:27 -0600

    first commit

---
+++ src/app/Components/filters-component/filters-component.component.ts
@@ -0,0 +1,66 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { MatFormFieldControl } from '@angular/material/form-field';
+
+export interface FilterItem {
+  filterId: string,
+  type: 'string' | 'number' | 'date' | 'boolean',
+  label?: string,
+  value?: boolean |string | number | Date | any,
+  onFilterChange: EventEmitter<any> | any,
+}
+@Component({
+  selector: 'app-filters-component',
+  templateUrl: './filters-component.component.html',
+  styleUrls: ['./filters-component.component.css'],
+  providers: [
+    { provide: MatFormFieldControl, useExisting: FiltersComponentComponent }   
+  ]
+})
+export class FiltersComponentComponent implements OnInit {
+
+  @Input() filtersList: FilterItem[] = [];
+
+  @Output() filterEvent = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onChangeFilter(event: Event | any, filter: FilterItem){
+    if(filter.type === 'string'){
+      this.filterEvent.emit(filter.onFilterChange(filter.filterId, (event.target as HTMLInputElement).value ));
+    }
+
+    if(filter.type === 'number'){
+      this.filterEvent.emit(filter.onFilterChange(filter.filterId, (event.target as HTMLInputElement).value ));
+    }
+
+    if(filter.type === 'boolean'){
+      this.filterEvent.emit(filter.onFilterChange(filter.filterId, event.checked ));
+    }
+
+    if(filter.type === 'date'){
+      this.filterEvent.emit(filter.onFilterChange(filter.filterId, new Date(event.value) ));
+    }
+  }
+
+  // onKeyPressStringFilter(eventText: any, event: any){
+  //   this.filterEvent.emit(event(textFilter));
+  // }
+
+  // onKeyPressNumberFilter(numberValueFilter: any){
+  //   this.filterEvent.emit(numberValueFilter);
+  // }
+
+  // onKeyPressDateFilter(dateFilter: any){
+  //   this.filterEvent.emit(dateFilter);
+  // }
+
+  // onKeyPressBooleanFilter(boolValueFilter: any){
+  //   this.filterEvent.emit(boolValueFilter);
+  // }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FilterItem } from './Components/filters-component/filters-component.component'
+
+interface TaskItem {
+  id: number
+  description: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'resusable-components-angular-v1';
+  title = 'resusable-components-angular-v1'
+
+  filtersList: FilterItem[] = [
+    {
+      filterId: 'nombre',
+      type: 'string',
+      label: 'Nombre',
+      value: null,
+      onFilterChange: (filterId: any, newValue: any) =>
+        this.applyChangesOnFilters(filterId, newValue),
+    },
+    {
+      filterId: 'edad',
+      type: 'number',
+      label: 'Edad',
+      value: 0,
+      onFilterChange: (filterId: any, newValue: any) =>
+        this.applyChangesOnFilters(filterId, newValue),
+    },
+    {
+      filterId: 'tieneVisa',
+      type: 'boolean',
+      label: 'Tiene Visa',
+      value: false,
+      onFilterChange: (filterId: any, newValue: any) =>
+        this.applyChangesOnFilters(filterId, newValue),
+    },
+    {
+      filterId: 'fechaNac',
+      type: 'date',
+      label: 'Fecha Nacimiento',
+      value: new Date(),
+      onFilterChange: (filterId: any, newValue: any) =>
+        this.applyChangesOnFilters(filterId, newValue),
+    },
+  ]
+
+  applyChangesOnFilters(
+    filterId: string,
+    newValue: any,
+  ) {
+    let indexItem = this.filtersList.findIndex(
+      (item) => item.filterId === filterId,
+    )
+    this.filtersList[indexItem].value = newValue
+  }
+
+  task: string
+
+  todolist: TaskItem[] = [
+    { id: 1, description: 'test' },
+    { id: 2, description: 'test' },
+  ]
+
+  removeItem(id: number) {
+    window.alert(id)
+    //this.todolist = this.todolist.filter(x => x.id!== id);
+  }
 }
